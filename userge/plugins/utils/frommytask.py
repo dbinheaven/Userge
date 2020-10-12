505edf0ff3c6e56b3f08b74fe6fe5d7b357f1db1
dbinheaven <36320167+dbinheaven@users.noreply.github.com>
2020-10-12 11:35:25 +0530

    Create frommytask.py

---
+++ userge/plugins/utils/frommytask.py
@@ -0,0 +1,19 @@
+#tasker filter for dbbots gro... keyword is frommytask
+
+ 
+
+from userge import userge, Message 
+
+from pyrogram import filters 
+
+ 
+
+@userge.on_message(filters.incoming & filters.chat(-1001151927732) & filters.regex(pattern=r"frommytask")) 
+
+async def xyz_abc(_, message: Message): 
+
+    txt = "/mirror@lzzy12_dbbot " 
+
+    txt += message.text 
+
+    await userge.send_message(message.chat.id, text=txt)
